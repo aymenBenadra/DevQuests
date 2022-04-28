8d8eda1ad5995783a4c7f7830ec9be282f11e014
Mohammed-Aymen Benadra <aymanbenadra16@gmail.com>
2022-04-28 12:48:06 +0000

    Updated Database to respond to project needs

--- Database/db_queries.sql
+++ Database/db_queries.sql
@@ -1,17 +1,17 @@
 CREATE DATABASE IF NOT EXISTS `devquests`;
 USE `devquests`;
 CREATE TABLE IF NOT EXISTS `users` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `id` int NOT NULL AUTO_INCREMENT,
     `username` varchar(255) NOT NULL,
     `name` varchar(255) NOT NULL,
     `email` varchar(255) NOT NULL,
     `password` varchar(255) NOT NULL,
     `avatar` varchar(255) NOT NULL,
-    `admin` boolean NOT NULL DEFAULT 0,
+    `is_admin` boolean NOT NULL DEFAULT 0,
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 CREATE TABLE IF NOT EXISTS `questions` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `id` int NOT NULL AUTO_INCREMENT,
     `question` varchar(255) NOT NULL,
     `answer` text NOT NULL,
     `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -19,44 +19,60 @@ CREATE TABLE IF NOT EXISTS `questions` (
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 CREATE TABLE IF NOT EXISTS `roadmaps` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `id` int NOT NULL AUTO_INCREMENT,
     `title` varchar(255) NOT NULL,
     `description` text NOT NULL,
-    /* Number of Hours needed*/
-    `time` int(11) NOT NULL,
-    `done` boolean NOT NULL,
     `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 CREATE TABLE IF NOT EXISTS `modules` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `id` int NOT NULL AUTO_INCREMENT,
     `title` varchar(255) NOT NULL,
     `description` text NOT NULL,
-    /* Number of Hours needed*/
-    `time` int(11) NOT NULL,
-    `done` boolean NOT NULL,
-    `roadmap_id` int(11) NOT NULL,
-    PRIMARY KEY (`id`),
-    FOREIGN KEY (`roadmap_id`) REFERENCES `roadmaps`(`id`)
+    `weeks` int NOT NULL,
+    PRIMARY KEY (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 CREATE TABLE IF NOT EXISTS `nodes` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `id` int NOT NULL AUTO_INCREMENT,
     `title` varchar(255) NOT NULL,
     `description` text NOT NULL,
-    /* Number of Hours needed*/
-    `time` int(11) NOT NULL,
-    `module_id` int(11) NOT NULL,
+    `order` int NOT NULL,
+    `module_id` int NOT NULL,
     PRIMARY KEY (`id`),
     FOREIGN KEY (`module_id`) REFERENCES `modules`(`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 CREATE TABLE IF NOT EXISTS `resources` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `id` int NOT NULL AUTO_INCREMENT,
     `title` varchar(255) NOT NULL UNIQUE,
     `description` text NOT NULL,
     `link` varchar(255) NOT NULL,
-    `visited` boolean NOT NULL DEFAULT 0,
+    `is_visited` boolean NOT NULL DEFAULT 0,
     `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+CREATE TABLE IF NOT EXISTS `roadmaps_modules` (
+    `roadmap_id` int NOT NULL,
+    `module_id` int NOT NULL,
+    `module_order` int NOT NULL,
+    PRIMARY KEY (`roadmap_id`, `module_id`),
+    FOREIGN KEY (`roadmap_id`) REFERENCES `roadmaps`(`id`),
+    FOREIGN KEY (`module_id`) REFERENCES `modules`(`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+CREATE TABLE IF NOT EXISTS `user_roadmaps` (
+    `user_id` int NOT NULL,
+    `roadmap_id` int NOT NULL,
+    `is_relaxed` boolean NOT NULL DEFAULT 1,
+    PRIMARY KEY (`user_id`, `roadmap_id`),
+    FOREIGN KEY (`user_id`) REFERENCES `users`(`id`),
+    FOREIGN KEY (`roadmap_id`) REFERENCES `roadmaps`(`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+CREATE TABLE IF NOT EXISTS `user_modules` (
+    `user_id` int NOT NULL,
+    `module_id` int NOT NULL,
+    `is_done` int NOT NULL DEFAULT 0,
+    PRIMARY KEY (`user_id`, `module_id`),
+    FOREIGN KEY (`user_id`) REFERENCES `users`(`id`),
+    FOREIGN KEY (`module_id`) REFERENCES `modules`(`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
