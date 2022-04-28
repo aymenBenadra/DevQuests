b3732a857ee490b72f08820e4e01626d88fac5ff
Mohammed-Aymen Benadra <aymanbenadra16@gmail.com>
2022-04-28 12:47:22 +0000

    optimized Request authorization method

--- core/helpers/Request.php
+++ core/helpers/Request.php
@@ -102,12 +102,10 @@ class Request
         // Get authorization token from header
         $auth = isset($_SERVER['Authorization']) && preg_match('/Bearer\s(\S+)/', $_SERVER['Authorization'], $matches) ? $matches[1] : null;
 
-        $auth ?? $auth = isset($_SERVER['HTTP_AUTHORIZATION']) && preg_match('/Bearer\s(\S+)/', $_SERVER['HTTP_AUTHORIZATION'], $matches) ? $matches[1] : null;
+        $auth ??= isset($_SERVER['HTTP_AUTHORIZATION']) && preg_match('/Bearer\s(\S+)/', $_SERVER['HTTP_AUTHORIZATION'], $matches) ? $matches[1] : null;
 
         // If no authorization token, get it from cookie
-        if (!$auth) {
-            $auth = isset($_COOKIE['jwt']) ? $_COOKIE['jwt'] : null;
-        }
+        $auth ??= isset($_COOKIE['jwt']) ? $_COOKIE['jwt'] : null;
 
         return $auth ?? false;
     }
