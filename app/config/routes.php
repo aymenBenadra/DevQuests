318f4adb2e4c6adf502fbe01401156b66e70eb0f
Mohammed-Aymen Benadra <aymanbenadra16@gmail.com>
2022-05-10 12:10:27 +0100

    Added Routes for API Gateway

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,38 +1,34 @@
 <?php
 
-/**
- * Routes Definition
- * 
- * ? Allowed Headers:
- * ? - Allowed origins: *
- * ? - Allowed methods: GET, POST, PUT, DELETE, OPTIONS
- * ? - Allowed headers: headerRef, Content-Type, Authorization, X-Requested-With
- * ? - Allowed credentials: true
- * ? - Max age: 86400
- * -----------------------------------------------------------------------------
- * ? Availlable Middlewares:
- * ? - Auth: check if user is logged in with "JWT" or "API KEY in Header" and has the right permissions to access the route
- * ? - Validation: Sanitize input data and check if it is valid (e.g. email, password, etc.)
- * -----------------------------------------------------------------------------
- * ? Auth MIDDLEWARE PARAMETERS:
- * ? - - guest: check if user is not logged in
- * ? - - user: check if user is logged in
- * ? - - admin: check if user is logged in and has the right permissions to access the route
- * ? - Validation MIDDLEWARE PARAMETERS:
- * ? - - field1|field2|field3: List of fields to check if they are valid (e.g. email, password, etc.)
- * -----------------------------------------------------------------------------
- * ? Example:
- * ? - GET ROUTE:
- * * $router->get('/', 'HomeController@index');
- * ? - POST ROUTE:
- * * $router->post('/', 'HomeController@index');
- * ? - PUT ROUTE:
- * * $router->put('/', 'HomeController@index');
- * ? - DELETE ROUTE:
- * * $router->delete('/', 'HomeController@index');
- * ? - ROUTE WITH MIDDLEWARES:
- * * $router->get('/', 'HomeController@index', ['Auth@guest', Validation@email|password|etc.']);
- * 
- * @package App\Config
- * @author Mohammed-Aymen Benadra
- */
+// Auth Routes
+$router->post('register', 'Auth@register', ['Auth@guest', 'Validation@username|name|email|password']); //*🚀
+$router->post('register/admin', 'Auth@registerAdmin', ['Auth@guest', 'Validation@username|name|email|password']); //*🚀
+$router->post('login', 'Auth@login', ['Auth@guest', 'Validation@login|password']); //*🚀
+
+// Resources Routes
+$router->get('resources', 'Resources@index', ['Auth@client']); //*🚀
+$router->get('resource', 'Resources@show', ['Auth@client', 'Validation@resource_id']); //*🚀
+$router->post('resource', 'Resources@store', ['Auth@client', 'Validation@title|description|link']); //*🚀
+$router->post('resource/update', 'Resources@update', ['Auth@client', 'Validation@resource_id|title|description|link']); //*🚀
+$router->post('resource/visited', 'Resources@toggleVisited', ['Auth@client', 'Validation@resource_id']); //*🚀
+$router->post('resource/delete', 'Resources@destroy', ['Auth@client', 'Validation@resource_id']); //*🚀
+
+// Interview Questions Routes
+$router->get('questions', 'Questions@index', ['Auth@client']); //*🚀
+$router->get('question', 'Questions@show', ['Auth@client', 'Validation@id']); //*🚀
+$router->post('question', 'Questions@store', ['Auth@client', 'Validation@question|answer']); //*🚀
+$router->post('question/update', 'Questions@update', ['Auth@client', 'Validation@id|question|answer']); //*🚀
+$router->post('question/delete', 'Questions@destroy', ['Auth@client', 'Validation@id']); //*🚀
+
+// Modules Routes
+$router->get('module', 'Modules@show', ['Auth@client', 'Validation@id']); //*🚀
+$router->post('module/completed', 'Modules@toggleCompleted', ['Auth@client', 'Validation@id']); //*🚀
+
+// Roadmaps Routes
+$router->get('roadmaps', 'Roadmaps@index', ['Auth@client']); //*🚀
+$router->get('roadmap/modules', 'Roadmaps@getModules', ['Auth@client', 'Validation@id']); //*🚀
+$router->get('roadmap/status', 'Roadmaps@status', ['Auth@client', 'Validation@id']); //*🚀
+$router->get('roadmap', 'Roadmaps@show', ['Auth@client', 'Validation@id']); //*🚀
+$router->post('roadmap', 'Roadmaps@store', ['Auth@client', 'Validation@title|description|modules']); //*🚀
+$router->post('roadmap/relaxed', 'Roadmaps@toggleRelaxed', ['Auth@client', 'Validation@id']); //*🚀
+$router->post('roadmap/delete', 'Roadmaps@destroy', ['Auth@client', 'Validation@id']); //*🚀
