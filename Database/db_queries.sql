bae7049223fed19515ed04742453283a3531ccbc
Mohammed-Aymen Benadra <aymanbenadra16@gmail.com>
2022-05-11 14:54:49 +0100

    removed unnecessary column from Resources table

--- Database/db_queries.sql
+++ Database/db_queries.sql
@@ -50,7 +50,6 @@ CREATE TABLE IF NOT EXISTS `resources` (
     `title` varchar(255) NOT NULL UNIQUE,
     `description` text NOT NULL,
     `link` varchar(255) NOT NULL,
-    `is_visited` boolean NOT NULL DEFAULT 0,
     `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`)
