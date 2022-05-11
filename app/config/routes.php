404abbd8da310d4a57687958bf6c7dc524424503
Mohammed-Aymen Benadra <aymanbenadra16@gmail.com>
2022-05-11 14:56:29 +0100

    Added @Scope for validation scope processing for more accurate validation and data processing

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,34 +1,33 @@
 <?php
 
 // Auth Routes
-$router->post('register', 'Auth@register', ['Auth@guest', 'Validation@username|name|email|password']); //*🚀
-$router->post('register/admin', 'Auth@registerAdmin', ['Auth@guest', 'Validation@username|name|email|password']); //*🚀
-$router->post('login', 'Auth@login', ['Auth@guest', 'Validation@login|password']); //*🚀
+$router->post('register', 'Auth@register', ['Auth@guest', 'Validation@username|name|email|password@user']); //*🚀
+$router->post('register/admin', 'Auth@registerAdmin', ['Auth@guest', 'Validation@username|name|email|password@user']); //*🚀
+$router->post('login', 'Auth@login', ['Auth@guest', 'Validation@login|password@user']); //*🚀
 
 // Resources Routes
-$router->get('resources', 'Resources@index', ['Auth@client']); //*🚀
-$router->get('resource', 'Resources@show', ['Auth@client', 'Validation@resource_id']); //*🚀
-$router->post('resource', 'Resources@store', ['Auth@client', 'Validation@title|description|link']); //*🚀
-$router->post('resource/update', 'Resources@update', ['Auth@client', 'Validation@resource_id|title|description|link']); //*🚀
-$router->post('resource/visited', 'Resources@toggleVisited', ['Auth@client', 'Validation@resource_id']); //*🚀
-$router->post('resource/delete', 'Resources@destroy', ['Auth@client', 'Validation@resource_id']); //*🚀
+$router->get('resources', 'Resources@index'); //*🚀
+$router->get('resource', 'Resources@show', ['Validation@id@resource']); //*🚀
+$router->post('resource', 'Resources@store', ['Auth@admin', 'Validation@title|description|link@resource']); //*🚀
+$router->post('resource/update', 'Resources@update', ['Auth@admin', 'Validation@id|title|description|link@resource']); //*🚀
+$router->post('resource/delete', 'Resources@destroy', ['Auth@admin', 'Validation@id@resource']); //*🚀
 
 // Interview Questions Routes
-$router->get('questions', 'Questions@index', ['Auth@client']); //*🚀
-$router->get('question', 'Questions@show', ['Auth@client', 'Validation@id']); //*🚀
-$router->post('question', 'Questions@store', ['Auth@client', 'Validation@question|answer']); //*🚀
-$router->post('question/update', 'Questions@update', ['Auth@client', 'Validation@id|question|answer']); //*🚀
-$router->post('question/delete', 'Questions@destroy', ['Auth@client', 'Validation@id']); //*🚀
+$router->get('questions', 'Questions@index'); //*🚀
+$router->get('question', 'Questions@show', ['Validation@id@question']); //*🚀
+$router->post('question', 'Questions@store', ['Auth@admin', 'Validation@question|answer@question']); //*🚀
+$router->post('question/update', 'Questions@update', ['Auth@admin', 'Validation@id|question|answer@question']); //*🚀
+$router->post('question/delete', 'Questions@destroy', ['Auth@admin', 'Validation@id@question']); //*🚀
 
 // Modules Routes
-$router->get('module', 'Modules@show', ['Auth@client', 'Validation@id']); //*🚀
-$router->post('module/completed', 'Modules@toggleCompleted', ['Auth@client', 'Validation@id']); //*🚀
+$router->get('module', 'Modules@show', ['Auth@client', 'Validation@id@module']); //*🚀
+$router->post('module/completed', 'Modules@toggleCompleted', ['Auth@client', 'Validation@id@module']); //*🚀
 
 // Roadmaps Routes
-$router->get('roadmaps', 'Roadmaps@index', ['Auth@client']); //*🚀
-$router->get('roadmap/modules', 'Roadmaps@getModules', ['Auth@client', 'Validation@id']); //*🚀
-$router->get('roadmap/status', 'Roadmaps@status', ['Auth@client', 'Validation@id']); //*🚀
-$router->get('roadmap', 'Roadmaps@show', ['Auth@client', 'Validation@id']); //*🚀
-$router->post('roadmap', 'Roadmaps@store', ['Auth@client', 'Validation@title|description|modules']); //*🚀
-$router->post('roadmap/relaxed', 'Roadmaps@toggleRelaxed', ['Auth@client', 'Validation@id']); //*🚀
-$router->post('roadmap/delete', 'Roadmaps@destroy', ['Auth@client', 'Validation@id']); //*🚀
+$router->get('roadmaps', 'Roadmaps@index@roadmap'); //*🚀
+$router->get('roadmap/modules', 'Roadmaps@getModules', ['Auth@client', 'Validation@id@roadmap']); //*🚀
+$router->get('roadmap/status', 'Roadmaps@status', ['Auth@client', 'Validation@id@roadmap']); //*🚀
+$router->get('roadmap', 'Roadmaps@show', ['Auth@client', 'Validation@id@roadmap']); //*🚀
+$router->post('roadmap', 'Roadmaps@store', ['Auth@client', 'Validation@title|description|modules@roadmap']); //*🚀
+$router->post('roadmap/relaxed', 'Roadmaps@toggleRelaxed', ['Auth@client', 'Validation@id@roadmap']); //*🚀
+$router->post('roadmap/delete', 'Roadmaps@destroy', ['Auth@client', 'Validation@id@roadmap']); //*🚀
