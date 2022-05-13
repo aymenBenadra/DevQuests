91bee17d976c592fec79eeb2aeb2f5522472cb20
Mohammed-Aymen Benadra <aymanbenadra16@gmail.com>
2022-05-13 11:00:52 +0100

    Organized routing and made it prettier

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,33 +1,58 @@
 <?php
 
-// Auth Routes
+/**
+ ** Auth Routes
+ */
+//? Guest
 $router->post('register', 'Auth@register', ['Auth@guest', 'Validation@username|name|email|password@user']); //*🚀
 $router->post('register/admin', 'Auth@registerAdmin', ['Auth@guest', 'Validation@username|name|email|password@user']); //*🚀
 $router->post('login', 'Auth@login', ['Auth@guest', 'Validation@login|password@user']); //*🚀
 
-// Resources Routes
+/**
+ ** Resources Routes
+ */
+//? Guest
 $router->get('resources', 'Resources@index'); //*🚀
 $router->get('resource', 'Resources@show', ['Validation@id@resource']); //*🚀
+
+//? Admin
 $router->post('resource', 'Resources@store', ['Auth@admin', 'Validation@title|description|link@resource']); //*🚀
 $router->post('resource/update', 'Resources@update', ['Auth@admin', 'Validation@id|description|link@resource']); //*🚀
 $router->post('resource/delete', 'Resources@destroy', ['Auth@admin', 'Validation@id@resource']); //*🚀
 
-// Interview Questions Routes
+/**
+ ** Interview Questions Routes
+ */
+//? Guest
 $router->get('questions', 'Questions@index'); //*🚀
 $router->get('question', 'Questions@show', ['Validation@id@question']); //*🚀
+
+//? Admin
 $router->post('question', 'Questions@store', ['Auth@admin', 'Validation@question|answer@question']); //*🚀
 $router->post('question/update', 'Questions@update', ['Auth@admin', 'Validation@id|answer@question']); //*🚀
 $router->post('question/delete', 'Questions@destroy', ['Auth@admin', 'Validation@id@question']); //*🚀
 
-// Modules Routes
-$router->get('module', 'Modules@show', ['Auth@client', 'Validation@id@module']); //*🚀
+/**
+ ** Modules Routes
+ */
+//? Guest
+$router->get('module', 'Modules@show', ['Validation@id@module']); //*🚀
+
+//? Client
 $router->post('module/completed', 'Modules@toggleCompleted', ['Auth@client', 'Validation@id@module']); //*🚀
 
-// Roadmaps Routes
-$router->get('roadmaps', 'Roadmaps@index@roadmap'); //*🚀
-$router->get('roadmap/modules', 'Roadmaps@getModules', ['Auth@client', 'Validation@id@roadmap']); //*🚀
+/**
+ ** Roadmaps Routes
+ */
+//? Guest
+$router->get('roadmaps', 'Roadmaps@index'); //*🚀
+$router->get('roadmap', 'Roadmaps@show', ['Validation@id@roadmap']); //*🚀
+$router->get('roadmap/modules', 'Roadmaps@getModules', ['Validation@id@roadmap']); //*🚀
+
+//? Client
 $router->get('roadmap/status', 'Roadmaps@status', ['Auth@client', 'Validation@id@roadmap']); //*🚀
-$router->get('roadmap', 'Roadmaps@show', ['Auth@client', 'Validation@id@roadmap']); //*🚀
-$router->post('roadmap', 'Roadmaps@store', ['Auth@client', 'Validation@title|description|modules@roadmap']); //*🚀
 $router->post('roadmap/relaxed', 'Roadmaps@toggleRelaxed', ['Auth@client', 'Validation@id@roadmap']); //*🚀
-$router->post('roadmap/delete', 'Roadmaps@destroy', ['Auth@client', 'Validation@id@roadmap']); //*🚀
+
+//? Admin
+$router->post('roadmap', 'Roadmaps@store', ['Auth@admin', 'Validation@title|description|modules@roadmap']); //*🚀
+$router->post('roadmap/delete', 'Roadmaps@destroy', ['Auth@admin', 'Validation@id@roadmap']); //*🚀
